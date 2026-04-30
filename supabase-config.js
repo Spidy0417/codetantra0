f1ecb8b212233937e7f354180627f05a8a404cc4
Praveen <twofungamersarmy@gmail.com>
2026-04-30 16:46:04 +0530

    Fix localPrograms array to include all 20 Java programs, remove Supabase dependencies

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-window.CODETANTRA_SUPABASE = {
-    url: "https://qofwzflbshzeytzofukv.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvZnd6Zmxic2h6ZXl0em9mdWt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MjgyNDAsImV4cCI6MjA5MzEwNDI0MH0.urTvYA03Q8QBfaRw1uO8GYxsb-MvdavW0oTNGyyZljg",
-    table: "programs",
-    defaultProgramSlug: "generate-electricity-bill"
-};
